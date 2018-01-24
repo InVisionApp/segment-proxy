a5ff6bbdddc6a7305f9f0f63c1696b1ca0525faf
Kai Rikhye <kai@invisionapp.com>
2018-01-24 18:17:43 -0500

    serve ajs-proxy.min.js

--- main.go
+++ main.go
@@ -1,32 +1,47 @@
 package main
 
 import (
-    "encoding/json"
-    "log"
-    "net/http"
-    "net/http/httputil"
-    "net/url"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
 )
- 
-func main() { 
-    http.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
-        if r.Method != "GET" {
-            w.WriteHeader(http.StatusMethodNotAllowed)
-            return
-        }
 
-        w.Header().Set("Content-Type", "application/json")
-        res := struct{ Status string }{"ok"}
-        jData, _ := json.Marshal(&res)
+func main() {
+	http.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
 
-        w.WriteHeader(http.StatusOK)
-        w.Write(jData)
-    })
- 
-    u, _ := url.Parse("https://api.segment.io/")
-    http.Handle("/", httputil.NewSingleHostReverseProxy(u))
- 
-    // Start the server
-    log.Printf("Initializing Proxy")
-    log.Fatal(http.ListenAndServe(":80", nil))
+		w.Header().Set("Content-Type", "application/json")
+		res := struct{ Status string }{"ok"}
+		jData, _ := json.Marshal(&res)
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(jData)
+	})
+
+	http.HandleFunc("/ajs-proxy.min.js", func(w http.ResponseWriter, r *http.Request) {
+		snippet, err := ioutil.ReadFile("static/ajs-proxy.min.js")
+		os.Stdout.Write(snippet)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "text/javascript")
+		w.Write(snippet)
+	})
+
+	u, _ := url.Parse("https://api.segment.io/")
+	http.Handle("/", httputil.NewSingleHostReverseProxy(u))
+
+	// Start the server
+	log.Printf("Initializing Proxy")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
